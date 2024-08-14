5e988852ac49b0363d1fb98317e16802ed532812
Jay <joelogira@gmail.com>
2024-08-14 22:03:57 +0300

    Added new configuration

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import Image from "next/image";
+import signInImage from "../../assets/sign-in-image.webp";
+import SignInForm from "./sign-in-form";
+import Link from "next/link";
 
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  return (
+    <main className="h-screen flex items-center justify-center p-5">
+      <div className="flex overflow-hidden h-full max-h-[40rem] w-full max-w-[64rem] bg-card shadow-2xl rounded-2xl">
+        <div className="overflow-y-auto p-10 w-full space-y-5 md:w-1/2 ">
+          <h1 className="text-3xl text-center font-bold text-primary">
+            Sign In to bugbook
+          </h1>
+          <div className="space-y-5">
+            <SignInForm />
+
+            <Link
+              href="/signup"
+              className="block text-center text-sm hover:underline"
+            >
+              Don&apos;t have an account? Sign Up
+            </Link>
+          </div>
+        </div>
+        <Image
+          src={signInImage}
+          alt="Sign In Image"
+          className="hidden italic object-cover w-1/2 md:block"
+          placeholder="blur"
+        />
+      </div>
+    </main>
+  );
 }
