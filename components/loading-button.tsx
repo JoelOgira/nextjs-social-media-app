1be4d5f6ae131dcae93c3ad874629d2c9dc3a22e
Jay <joelogira@gmail.com>
2024-08-15 20:52:18 +0300

    Created loading button

---
+++ components/loading-button.tsx
@@ -0,0 +1,20 @@
+import { Loader2 } from "lucide-react";
+import { Button, ButtonProps } from "./ui/button";
+
+interface LoadingButtonProps extends ButtonProps {
+  loading: boolean;
+}
+
+export default function LoadingButton({
+  loading,
+  disabled,
+  className,
+  ...props
+}: LoadingButtonProps) {
+  return (
+    <Button className="rounded-full w-full">
+      {loading && <Loader2 className="animate-spin size-5" />}
+      {props.children}
+    </Button>
+  );
+}
