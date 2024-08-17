fadf594baafee386afe3c8d9d025f5e6910ad92a
Jay <joelogira@gmail.com>
2024-08-17 05:40:15 +0300

    Created search field

---
+++ app/(timeline)/_components/search-field.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { SearchFormValues, searchSchema } from "@/lib/validation";
+import { SearchInput } from "@/components/search-input";
+import { useSearchContext } from "../_providers/search-context";
+
+export default function SearchField() {
+  const router = useRouter();
+  const { isMobile, setShowSearch } = useSearchContext();
+
+  const form = useForm<SearchFormValues>({
+    resolver: zodResolver(searchSchema),
+    defaultValues: {
+      q: "",
+    },
+  });
+
+  function onSubmit(values: SearchFormValues) {
+    router.push(`/search?q=${encodeURIComponent(values.q)}`);
+    if (isMobile) {
+      setShowSearch(false);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="q"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <SearchInput {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
