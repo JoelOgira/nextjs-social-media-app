b3bb0d8643a2d6502ad9ff8f81c7508f4f444d3a
Jay <joelogira@gmail.com>
2024-08-22 03:25:00 +0300

    Added post editor

--- app/(timeline)/page.tsx
+++ app/(timeline)/page.tsx
@@ -1,5 +1,11 @@
-
+import PostEditor from "@/components/posts/editor/post-editor";
 
 export default function Home() {
-  return <main className="">Welcome to your timeline</main>;
+  return (
+    <main className="flex min-w-0 w-full justify-center">
+      <div className="min-w-0 w-full space-y-5">
+        <PostEditor />
+      </div>
+    </main>
+  );
 }
