45548aa8f87605f393c248b19705bc0816429d53
Jay <joelogira@gmail.com>
2024-08-22 03:20:17 +0300

    Modified site title and changed site tile

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -12,9 +12,9 @@ export default function SignInPage() {
   return (
     <main className="h-screen flex items-center justify-center p-5">
       <div className="flex overflow-hidden h-fit max-h-[40rem] w-full max-w-[64rem] bg-card shadow-2xl rounded-2xl">
-        <div className="overflow-y-auto p-10 w-full space-y-5 md:w-1/2 ">
-          <h1 className="text-3xl text-center font-bold text-primary">
-            Sign In to bugbook
+        <div className="overflow-y-auto p-6 w-full space-y-5 md:w-1/2 ">
+          <h1 className="text-xl text-center font-bold text-primary sm:text-2xl md:text-3xl">
+            Sign In to gumzo
           </h1>
           <div className="space-y-5">
             <SignInForm />
