ad70d234472e8546043616f0f05071bd6f632688
Jay <joelogira@gmail.com>
2024-08-23 07:56:11 +0300

    created post component

---
+++ components/posts/editor/post.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import UserAvatar from "@/components/user-avatar";
+import { PostData } from "@/lib/types";
+import { formatRelativeDate } from "@/lib/utils";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+interface PostProps {
+  post: PostData;
+}
+
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
+
+  return (
+    <article
+      onClick={() => router.push(`/posts/${post.id}`)}
+      className="space-y-3 rounded-2xl bg-card p-3 shadow-sm"
+    >
+      <div className="flex gap-3">
+        <Link href={`/users/${post.user.username}`}>
+          <UserAvatar avatarUrl={post.user.avatarUrl} size={40} />
+        </Link>
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline"
+          >
+            {post.user.displayName}
+          </Link>
+          <Link
+            href={`/posts/${post.id}`}
+            className="block text-sm text-muted-foreground hover:underline"
+          >
+            {formatRelativeDate(post.createdAt)}
+          </Link>
+          <div className="whitespace-pre-line break-words">{post.content}</div>
+        </div>
+      </div>
+    </article>
+  );
+}
