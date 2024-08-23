04097509ded7242f46176819b077ff1722ac080f
Jay <joelogira@gmail.com>
2024-08-23 07:55:49 +0300

    Added posts

--- app/(timeline)/page.tsx
+++ app/(timeline)/page.tsx
@@ -1,10 +1,23 @@
+import Post from "@/components/posts/editor/post";
 import PostEditor from "@/components/posts/editor/post-editor";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: postDataInclude,
+  });
+
   return (
     <main className="flex min-w-0 w-full justify-center">
       <div className="min-w-0 w-full space-y-5">
         <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </main>
   );
