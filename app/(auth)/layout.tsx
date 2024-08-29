60b877a0f1658def235c6811d2a161525e233dc8
Jay <joelogira@gmail.com>
2024-08-29 16:04:34 +0300

    Added user validation and redirect

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,14 @@
-export default function layout({ children }: { children: React.ReactNode }) {
+import { validateRequest } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { user } = await validateRequest();
+
+  if (user) redirect("/");
+
   return <>{children}</>;
 }
