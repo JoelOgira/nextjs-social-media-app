f3c8383099a12a6931129d8e94044fae06d6ac4c
Jay <joelogira@gmail.com>
2024-08-30 07:54:44 +0300

    Updated search provider import statement

--- app/(timeline)/_components/navbar.tsx
+++ app/(timeline)/_components/navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import SearchField from "./search-field";
 import UserButton from "@/components/user-button";
 import { cn } from "@/lib/utils";
-import { useSearchContext } from "../_providers/search-context";
+import { useSearchContext } from "../_providers/search-provider";
 
 export default function Navbar() {
   const { isMobile, showSearch } = useSearchContext();
