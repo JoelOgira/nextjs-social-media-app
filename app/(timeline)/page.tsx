769d9ae15a20e3d9768987f5f067d3b534f69619
Jay <joelogira@gmail.com>
2024-08-30 07:57:02 +0300

    Added content to the page

--- app/(timeline)/page.tsx
+++ app/(timeline)/page.tsx
@@ -1,5 +1,26 @@
+import Post from "@/components/posts/editor/post";
+import PostEditor from "@/components/posts/editor/post-editor";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
+export default async function Timeline() {
+  const posts = await prisma.post.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: postDataInclude,
+  });
 
-export default function Home() {
-  return <main className="">Welcome to your timeline</main>;
+  return (
+    <main className="flex w-full min-w-0 justify-center">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        <div className="flex flex-col space-y-3">
+          {posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
