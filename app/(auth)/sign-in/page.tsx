faafb6d77eeb4d55224243e5c8fd33a33059ff85
Jay <joelogira@gmail.com>
2024-08-30 10:12:34 +0300

    Updated content and styling in the page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -12,16 +12,16 @@ export default function SignInPage() {
   return (
     <main className="h-screen flex items-center justify-center p-5">
       <div className="flex overflow-hidden h-fit max-h-[40rem] w-full max-w-[64rem] bg-card shadow-2xl rounded-2xl">
-        <div className="overflow-y-auto p-10 w-full space-y-5 md:w-1/2 ">
+        <div className="overflow-y-auto p-5 w-full space-y-5 md:w-1/2 lg:p-10">
           <h1 className="text-3xl text-center font-bold text-primary">
-            Sign In to bugbook
+            Sign In to gumzo
           </h1>
           <div className="space-y-5">
             <SignInForm />
 
             <Link
               href="/sign-up"
-              className="block text-center text-sm hover:underline"
+              className="block text-center text-muted-foreground text-sm hover:underline"
             >
               Don&apos;t have an account? Sign Up
             </Link>
