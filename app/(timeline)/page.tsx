8c3a869e9ccf53815f3259cebf838ac802d2bc4a
Jay <joelogira@gmail.com>
2024-09-01 21:46:31 +0300

    Modified the page to include for you feed

--- app/(timeline)/page.tsx
+++ app/(timeline)/page.tsx
@@ -1,26 +1,17 @@
-import Post from "@/components/posts/editor/post";
 import PostEditor from "@/components/posts/editor/post-editor";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import TrendsSidebar from "../../components/trend-sidebar";
+import ForYouFeed from "./_components/for-you-feed";
 
-export default async function Timeline() {
-  const posts = await prisma.post.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: postDataInclude,
-  });
-
+export default function Timeline() {
   return (
-    <main className="flex w-full min-w-0 justify-center">
+    <main className="flex w-full min-w-0 justify-center gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <div className="flex flex-col space-y-3">
-          {posts.map((post) => (
-            <Post key={post.id} post={post} />
-          ))}
-        </div>
+
+        <ForYouFeed />
       </div>
+
+      <TrendsSidebar />
     </main>
   );
 }
