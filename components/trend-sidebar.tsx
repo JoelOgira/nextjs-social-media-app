53ae0df61d294e2f6610338377a09d9224969199
Jay <joelogira@gmail.com>
2024-09-01 21:49:28 +0300

    created trends sidebar

---
+++ components/trend-sidebar.tsx
@@ -0,0 +1,108 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { userDataSelect } from "@/lib/types";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import React, { Suspense } from "react";
+import UserAvatar from "./user-avatar";
+import { Button } from "./ui/button";
+import { unstable_cache } from "next/cache";
+import { formatNumber } from "@/lib/utils";
+
+export default function TrendsSidebar() {
+  return (
+    <div className="hidden sticky top-[5.25rem] w-72 h-fit space-y-5 flex-none lg:block lg:w-80">
+      <Suspense fallback={<Loader2 className="size-5 animate-spin mx-auto" />}>
+        <WhoToFollow />
+        <TrendingTopics />
+      </Suspense>
+    </div>
+  );
+}
+
+async function WhoToFollow() {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const usersToFollow = await prisma.user.findMany({
+    where: {
+      NOT: {
+        id: user.id,
+      },
+    },
+    select: userDataSelect,
+    take: 5,
+  });
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Who to follow</div>
+      {usersToFollow.map((user) => (
+        <div
+          key={user.id}
+          className="flex items-center justify-between gap-3 w-full"
+        >
+          <Link
+            className="flex items-center gap-3"
+            href={`/users/${user.username}`}
+          >
+            <UserAvatar className="flex-none" avatarUrl={user.avatarUrl} />
+            <div className="[&>p]:line-clamp-1 [&>p]:break-all">
+              <p className="font-semibold hover:underline">
+                {user.displayName}
+              </p>
+              <p className="text-muted-foreground">@{user.username}</p>
+            </div>
+          </Link>
+          <Button>Follow</Button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const getTrendingTopics = unstable_cache(
+  async () => {
+    const result = await prisma.$queryRaw<{ hashtag: string; count: bigint }[]>`
+            SELECT LOWER(unnest(regexp_matches(content, '#[[:alnum:]_]+', 'g'))) AS hashtag, COUNT(*) AS count
+            FROM posts
+            GROUP BY (hashtag)
+            ORDER BY count DESC, hashtag ASC
+            LIMIT 5
+        `;
+
+    return result.map((row) => ({
+      hashtag: row.hashtag,
+      count: Number(row.count),
+    }));
+  },
+  ["trending_topics"],
+  {
+    revalidate: 3 * 60 * 60,
+  }
+);
+
+async function TrendingTopics() {
+  const trendingTopics = await getTrendingTopics();
+
+  return (
+    <div className="bg-card space-y-5 rounded-2xl p-5 shadow-sm">
+      <div className="text-xl font-bold">Trending Topics</div>
+      {trendingTopics.map(({ hashtag, count }) => {
+        const title = hashtag.split("#")[1];
+
+        return (
+          <Link className="block" key={title} href={`/hashtag/${title}`}>
+            <p className="line-clamp-1 break-all font-semibold hover:underline">
+              {hashtag}
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {formatNumber(count)} {count === 1 ? "post" : "posts"}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
